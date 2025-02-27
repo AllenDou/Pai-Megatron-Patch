9c79050ff9bff8537af8c65f36c61982ff3db32e
zixiao <shunli.dsl@alibaba-inc.com>
2025-02-27 10:56:10 +0800

    update.

--- 2_download_data.sh
+++ 2_download_data.sh
@@ -1,33 +1,33 @@
 cd /nasmnt/megatron
 mkdir -p qwen-ckpts
 cd qwen-ckpts
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-ckpts/Qwen2.5-0.5B.tar.gz
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-ckpts/Qwen2.5-0.5B.tar.gz -O Qwen2.5-0.5B.tar.gz
 tar -zxf Qwen2.5-0.5B.tar.gz
 
 cd /nasmnt/megatron
 mkdir -p qwen-datasets
 cd qwen-datasets
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-datasets/wudao_qwenbpe_text_document.bin
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-datasets/wudao_qwenbpe_text_document.idx
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-datasets/wudao_qwenbpe_text_document.bin -O wudao_qwenbpe_text_document.bin
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/qwen-datasets/wudao_qwenbpe_text_document.idx -O wudao_qwenbpe_text_document.idx
 
 # json微调数据集
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/deepseek-datasets/alpaca_zh-train-general.json
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/deepseek-datasets/alpaca_zh-valid-general.json
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/deepseek-datasets/alpaca_zh-train-general.json -O alpaca_zh-train-general.json
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/deepseek-datasets/alpaca_zh-valid-general.json -O alpaca_zh-valid-general.json
 
 # In container
 cd /nasmnt/megatron
 
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/evaluate.tgz
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/cmmlu.tgz
-wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/ceval.tgz
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/evaluate.tgz -O evaluate.tgz
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/cmmlu.tgz -O cmmlu.tgz
+wget https://atp-modelzoo-wlcb-pai.oss-cn-wulanchabu.aliyuncs.com/release/models/pai-megatron-patch/evaluation-datasets/ceval.tgz -O ceval.tgz
 
 tar -xvzf cmmlu.tgz
 tar -xvzf ceval.tgz
 tar -xvzf evaluate.tgz
 
-ln -s /nasmnt/megatron/cmmlu /workspace/
-ln -s /nasmnt/megatron/ceval /workspace/
-ln -s /nasmnt/megatron/evaluate /workspace/
+ln -sf /nasmnt/megatron/cmmlu /workspace/
+ln -sf /nasmnt/megatron/ceval /workspace/
+ln -sf /nasmnt/megatron/evaluate /workspace/
 
 #cd /workspace/
 #wget http://easynlp-dev.oss-cn-zhangjiakou.aliyuncs.com/225247/Pai-Megatron-Patch/code/evaluate.tgz
